ce471b57fd96ad579dee1a7c398332cbd1534a7f
Mark Gachoka <makunomark@gmail.com>
2020-01-02 00:49:53 +0300

    Dagger is set up!

---
+++ app/src/main/java/com/example/android_project_template/TemplateApplication.kt
@@ -0,0 +1,23 @@
+package com.example.android_project_template
+
+import android.app.Application
+import com.example.android_project_template.di.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+internal class TemplateApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun onCreate() {
+        super.onCreate()
+        DaggerApplicationComponent.create().inject(this)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
+    }
+}
---
+++ app/src/main/java/com/example/android_project_template/di/modules/ActivityModule.kt
@@ -0,0 +1,15 @@
+package com.example.android_project_template.di.modules
+
+import com.example.android_project_template.di.annotation.ActivityScope
+import com.example.android_project_template.ui.main.view.MainActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+
+@Module
+interface ActivityModule {
+
+    @ActivityScope
+    @ContributesAndroidInjector
+    fun provideMainActivity(): MainActivity
+}
